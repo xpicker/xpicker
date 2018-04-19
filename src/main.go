13c7261333d666f383f3581adf627c1708c4434c
LuGuoHuas <lu_guohua@163.com>
2018-04-19 18:34:57 +0800

    update: add endless to 10086

--- src/main.go
+++ src/main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/fvbock/endless"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	router := gin.Default()
+	test := router.Group("/api/test/")
+	{
+		test.GET("/ping", PingPongTest)
+	}
+	endless.ListenAndServe(":10086", router)
+}
+
+
+func PingPongTest(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
 	})
-	r.Run()
 }
